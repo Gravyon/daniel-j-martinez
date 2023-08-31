a5bd449bc150699d2d19e1641d7c8c479eb3cbfe
Gravyon <rdjmartinez95@gmail.com>
2023-08-31 11:17:54 -0300

    Adjusted logo position

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 const LanguageSwitcher = ({ currentLanguage, onChangeLanguage }) => {
   const lngs = {
-    en: { nativeName: "En" },
-    es: { nativeName: "Es" },
+    en: { nativeName: "EN" },
+    es: { nativeName: "ES" },
   };
   return (
-    <div className="ml-auto">
-      <select
-        className="text-gray-300 cursor-pointer nav-text rounded-md px-3 py-2 uppercase text-sm font-medium bg-transparent"
-        value={currentLanguage}
-        onChange={(e) => onChangeLanguage(e.target.value)}
-      >
-        {Object.keys(lngs).map((lng) => (
-          <option key={lng} value={lng}>
-            {lngs[lng].nativeName}
-          </option>
-        ))}
-      </select>
-    </div>
+    <select
+      className="text-gray-300 cursor-pointer nav-text rounded-md px-3 py-2 uppercase text-sm font-medium bg-transparent"
+      value={currentLanguage}
+      onChange={(e) => onChangeLanguage(e.target.value)}
+    >
+      {Object.keys(lngs).map((lng) => (
+        <option key={lng} value={lng}>
+          {lngs[lng].nativeName}
+        </option>
+      ))}
+    </select>
   );
 };
 
