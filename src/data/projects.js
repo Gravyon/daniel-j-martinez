e1db35763780ceed6e2272d31f46e7629243a690
Gravyon <rdjmartinez95@gmail.com>
2023-09-02 22:18:06 -0300

    Small fix to technologies

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,14 +7,7 @@ export const data = [
     descname: "devsafio",
     description:
       "A fullstack overhaul of Devsafio's main website where employers can request talent and applicants can find jobs",
-    tecnologies: [
-      "React",
-      "Node",
-      "Firebase",
-      "Typescript",
-      "TypeORM",
-      "Tailwind",
-    ],
+    tecnologies: ["React, Node, Firebase, Typescript, TypeORM,Tailwind"],
     image: devsafio,
     link: "https://c8-frontend.vercel.app/",
   },
@@ -23,7 +16,7 @@ export const data = [
     descname: "casaarte",
     description:
       "A full-stack interactive web application where users can buy and sell art, comment and rate others artwork",
-    tecnologies: ["React", "Node", "Python", "Flask", "Bootstrap"],
+    tecnologies: ["React, Node, Python, Flask, Bootstrap"],
     image:
       "https://camo.githubusercontent.com/5ce3da500733f2ea8f8aa89fd62b6b66890223fcb35aafbca596510a32a794aa/68747470733a2f2f692e696d6775722e636f6d2f42696f595a30732e706e67",
     link: "https://casa-arte-oz78.onrender.com/",
@@ -33,7 +26,7 @@ export const data = [
     descname: "reddit",
     description:
       "A clone of Reddit with a functioning login and register, using Firebase as the database with user communities and user feed",
-    tecnologies: ["NextJS", "Firebase", "TypeScript", "Chakra UI"],
+    tecnologies: ["NextJS, Firebase, TypeScript, Chakra UI"],
     image: reddit,
     link: "https://reddit-clone-gravyon.vercel.app/",
   },
@@ -42,7 +35,7 @@ export const data = [
     descname: "dreamview",
     description:
       "As a technical test, I was tasked with building a website for a fictional cinema theater where they would display the movies for viewing and users can review them",
-    tecnologies: ["React", "CSS", "Material UI"],
+    tecnologies: ["React, CSS, Material UI"],
     image: dreamview,
     link: "https://github.com/Gravyon/4geeks-final-project",
   },
