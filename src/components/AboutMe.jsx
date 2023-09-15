dd389a0f72942d1c601b689ec4e23071639b13d0
Gravyon <rdjmartinez95@gmail.com>
2023-09-15 20:30:09 -0300

    Added image of myself and some styling changes to about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,33 +1,40 @@
 import React from "react";
 import { Slide } from "react-awesome-reveal";
 import { Trans, useTranslation } from "react-i18next";
-
+import daniel from "../assets/daniel.jpg";
 const AboutMe = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="about-me mb-8">
+    <div>
       <Slide triggerOnce delay={100} direction="down" duration={1000}>
         <h1 className="flex m-4 p-2">
           <Trans i18nKey="aboutme">About me</Trans>
         </h1>
       </Slide>
-      <div className="about-me-content">
-        <Slide triggerOnce direction="left">
-          <p className="about-me-text">
-            <Trans i18nKey="info1"></Trans>
-          </p>
-        </Slide>
-        <Slide triggerOnce direction="right">
-          <p className="about-me-text m-2">
-            <Trans i18nKey="info2"></Trans>
-          </p>
-        </Slide>
-        <Slide triggerOnce direction="left">
-          <p className="about-me-text m-2">
-            <Trans i18nKey="info3"></Trans>
-          </p>
-        </Slide>
+      <img
+        src={daniel}
+        alt="Yo"
+        className="photo w-40 mt-10 sm:inline sm:float-right"
+      />
+      <div className="about-me">
+        <div className="about-me-content">
+          <Slide triggerOnce direction="left">
+            <p className="about-me-text m-5 sm:m-2">
+              <Trans i18nKey="info1"></Trans>
+            </p>
+          </Slide>
+          <Slide triggerOnce direction="right">
+            <p className="about-me-text m-5 sm:m-2">
+              <Trans i18nKey="info2"></Trans>
+            </p>
+          </Slide>
+          <Slide triggerOnce direction="left">
+            <p className="about-me-text m-5 sm:m-2">
+              <Trans i18nKey="info3"></Trans>
+            </p>
+          </Slide>
+        </div>
       </div>
     </div>
   );
